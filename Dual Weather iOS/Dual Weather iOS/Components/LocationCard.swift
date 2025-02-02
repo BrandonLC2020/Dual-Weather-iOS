fcd9fe72eb5ecde7d83d7e55b28b461973e0bce2
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2025-02-02 12:14:17 -0500

    Create LocationCard.swift

---
+++ Dual Weather iOS/Dual Weather iOS/Components/LocationCard.swift
@@ -0,0 +1,18 @@
+//
+//  LocationCard.swift
+//  Dual Weather iOS
+//
+//  Created by Brandon Lamer-Connolly on 2/2/25.
+//
+
+import SwiftUI
+
+struct LocationCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationCard()
+}
