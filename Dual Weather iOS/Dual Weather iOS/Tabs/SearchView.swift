134c1fb4834533828cfad85d60b3d50d401bdfd3
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2025-02-03 12:03:32 -0500

    Create SearchView.swift

---
+++ Dual Weather iOS/Dual Weather iOS/Tabs/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  Dual Weather iOS
+//
+//  Created by Brandon Lamer-Connolly on 2/3/25.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("Coming soon...")
+    }
+}
+
+#Preview {
+    SearchView()
+}
